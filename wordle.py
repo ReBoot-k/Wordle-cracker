1f74e7ae21133e3fd6fe833b459a7eb939e60bf1
ReBoot-k <131132246+ReBoot-k@users.noreply.github.com>
2023-11-08 16:46:47 +0300

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -6,35 +6,50 @@ import base64
 import re
 import urllib.parse
 
-PATTERN_ONE = re.compile("(https://wordleplay\.com/.+/\?challenge=?)(?P<word>.+)")
-PATTERN_TWO = re.compile("(https://wordleplay\.com/.+/\?tour=?)(?P<word>.+)")
-
-def decode_url(string):
-    result = urllib.parse.unquote(string)
-    return result
+class WordleCracker:
+    def __init__(self, pattern="(https://wordleplay\.com/.+/\?(challenge|tour)=?)(?P<word>.+)") -> None:
+        self.pattern = re.compile(pattern)
 
 
-def decode_base64(string):
-    encode = string.encode()
-    decode = base64.b64decode(encode)
-    result = decode.decode()
-    return result
+    def decode_url(self, string) -> str:
+        result = urllib.parse.unquote(string)
+        return result
 
+
+    def decode_base64(self, string) -> str:
+        encode = string.encode()
+        decode = base64.b64decode(encode)
+        result = decode.decode()
+        return result
+
+
+    def extract_encode_string(self, link) -> dict:
+        data_regex = self.pattern.match(link)
+        if data_regex:
+            data_string = data_regex.group("word")
+            data_normalize = data_regex.group(1) + self.decode_url(data_string)
+            return {"word_base64": data_string, "normalize": data_normalize}
+        return None
+
+    def crack(self, link) -> str:
+        extract = self.extract_encode_string(link)
+        if extract:
+            data_normalize = extract["normalize"]
+
+            data = self.decode_base64(extract['word_base64'])
+            data = data.replace(";", ", ")
+
+
+            result = "Ответ: {}\nНормализованная ссылка: {}\n\n".format(data, data_normalize)
+        else:
+            result = None
+
+        return result
 
 print("Cracker wordleplay.com\n")
+cracker = WordleCracker()
+
 while True:
     link = input("Введите ссылку: ")
-
-    data_regex = PATTERN_ONE.match(link) or PATTERN_TWO.match(link)
-    if data_regex:
-        data_string = data_regex.group("word")
-        data_normalize = decode_url(data_string)
-        data = decode_base64(data_normalize)
-        if ";" in data:
-            data = ", ".join(data.split(";"))
-
-        result = "Ответ: {}\nНормализованная ссылка: {}\n".format(data, data_regex.group(1) + urllib.parse.quote(data_string))
-    else:
-        result = "Не понимаю. Блокирую!"
-
+    result = cracker.crack(link) or "Не понимаю. Блокирую!"
     print(result)
